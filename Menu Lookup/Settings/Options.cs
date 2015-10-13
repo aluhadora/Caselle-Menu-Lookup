b712f5fe2a182efc08b0f0d5714a29e7a9f1e2a5
Allan T. Harper <ath@caselle.com>
2015-10-13 16:47:08 -0600

    Added .gitignore

--- Menu Lookup/Settings/Options.cs
+++ Menu Lookup/Settings/Options.cs
@@ -2,8 +2,8 @@
 {
   public static class Options
   {
-    //public const string CodeFolder = @"D:\CodeNet\Connect3\";    //ATH
-    public const string CodeFolder = @"D:\CodeNet\Development\";   //JON & BSS
+    public const string CodeFolder = @"D:\CodeNet\Connect\";    //ATH
+    //public const string CodeFolder = @"D:\CodeNet\Development\";   //JON & BSS
     //public const string CodeFolder = @"D:\CodeNet\Connect\";     //BPS
     //public const string CodeFolder = @"D:\Code\Connect\";        //JCS
   }
